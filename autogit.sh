649cf90e0142d60c01cd1b181a87e1d4afed6fd8
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-04-03 22:25:20 -0300

    tarefa 01 -> feita

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A
 git commit -m "tarefa 01 -> feita"
 git add -A
-git commit -m "atividade 01 -> feita"
+git commit -m "fix -> index.html images | remove -> frontend folder"
 git push -u origin main
 git status
